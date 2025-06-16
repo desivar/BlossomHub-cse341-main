02c150c51b5e602e9452b9e12a34be1e69e6ebfb
desivar <jillian_desire@yahoo.es>
2025-06-16 11:54:10 -0600

    auth

---
+++ BlossomHub-cse341-main/middleware/isAuthenticated.js
@@ -0,0 +1,10 @@
+function isAuthenticated(req, res, next) {
+  if (!req?.session?.user) {
+    return res.status(401).json({
+      message: "Unauthorized"
+    })
+  }
+  next()
+};
+
+module.exports = { isAuthenticated };
